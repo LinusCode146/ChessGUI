3456d6a6d0e84e89583e6b941f9eb7e78063e952
Linus Schulze <linus.schulze@denkmit.de>
2023-02-03 20:38:23 +0100

    rendered pieces and bg at correct position

---
+++ game.py
@@ -0,0 +1,34 @@
+import pygame
+
+from consts import *
+from board import Board
+
+
+class Game:
+
+    def __init__(self) -> None:
+        self.board = Board()
+
+    def show_bg(self, surface) -> None:
+        for row in range(ROWS):
+            for col in range(COLS):
+                if (row + col) % 2 == 0:
+                    color = (234, 235, 200)  # lightgreen
+                else:
+                    color = (119, 154, 88)  # dark green
+
+                rect = (col * SQR_SIZE, row * SQR_SIZE, SQR_SIZE, SQR_SIZE)
+
+                pygame.draw.rect(surface, color, rect)
+
+    def show_pieces(self, surface):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # piece ?
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQR_SIZE + SQR_SIZE // 2, row * SQR_SIZE + SQR_SIZE // 2
+                    piece.texture_rect = img.get_rect(center=img_center)
+                    surface.blit(img, piece.texture_rect)
